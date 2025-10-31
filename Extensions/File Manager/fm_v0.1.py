6ca87fff1b527328b61ed1337c9d0f1073c8c52f
Pratschi <pratschigamer@gmail.com>
2025-10-31 16:52:04 +0100

    Add File manager v0.1

---
+++ Extensions/File Manager/fm_v0.1.py
@@ -0,0 +1,28 @@
+# PachTool Extension /fm/Pratschi/1.0.0/Pratchi's File Manager/
+
+print("Loading File Manager...")
+import os, json
+
+currpath = os.getcwd()
+
+while True:
+    cmd = input(f"\n[FM] {currpath} ~ ")
+    if cmd.startswith("cd"):
+        if len(cmd.split(" ")) == 1:
+            currpath = os.getcwd()
+        else:
+            if cmd.split(" ")[1] == "..":
+                currpath = os.path.dirname(currpath)
+            elif os.path.isdir(os.path.join(currpath, cmd.split(" ")[1])):
+                currpath = os.path.join(currpath, cmd.split(" ")[1])
+            elif os.path.isfile(os.path.join(currpath, cmd.split(" ")[1])):
+                print(f"[FM] ERROR: {cmd.split(' ')[1]} Is a file!")
+            else:
+                print(f"[FM] ERROR: {cmd.split(' ')[1]} Folder doesn't exist!")
+    elif cmd.startswith("ls"):
+        for i in os.listdir(currpath):
+            print(f"    {i}{' (Folder)' if os.path.isdir(os.path.join(currpath, i)) else ''}")
+    elif cmd.startswith("quit"):
+        break
+    else:
+        print("[FM] ERROR: Unknown command!")
